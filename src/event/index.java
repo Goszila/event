3848eed80cb77be52d477bf48af66d16fcd3eedb
NEXT-LKA4SJFK8L\NEXT Speed <changprss@gmail.com>
2017-09-14 09:37:30 +0700

    event

--- src/event/index.java
+++ src/event/index.java
@@ -1,10 +1,20 @@
 package event;
-
+import java.util.Scanner; 
 public class index {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Input number");
+		int number,num;
+		number =keyboard.nextInt();
+		num= number%2;
+
+		if (num ==0)
+		System.out.println("àÅ¢¤Ùè");
+		else
+		System.out.println("àÅª¤Õè");
+		}
 
 	}
 
-}
